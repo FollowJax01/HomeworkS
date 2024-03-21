c624ed81285d470e32e59e4943dd1b35d026e5bb
FollowJax <sheva9a@gmail.com>
2024-03-21 10:41:39 +0100

    main update

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,41 @@
-void Homework2();
+void homework2();
 void Homework003();
-
+int Homework001();
+int Homework004();
+int test();
+int test1();
+int test2();
+int test3();
+void test4();
+void test5();
+void test6();
+void test8();
+int mapTest();
+int test9();
+void inherTest01();
+int homework10();
+void inherTest02();
+int observe();
+int observer2();
 
 int main() {
-	 // Homework2();
-	Homework003();
+	//homework2();
+	//Homework003();
+	//Homework001();
+	//Homework004();
+	//test();
+	//test1();
+	//test2();
+	//test3();
+	//test4();
+	//test5();
+	//test6();
+	//mapTest();
+	//test8();
+	//test9();
+	homework10();
+	// inherTest01();
+	//inherTest02();
+	//observe();
+	//observer2();
 }
